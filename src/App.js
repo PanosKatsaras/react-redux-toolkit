65d0883a77f52f8323f6d3d518392bbc39923461
pan1000 <pan.katsaras@gmail.com>
2024-11-27 10:58:06 +0200

    Updated app structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Cart from './components/Cart';
 import RootLayout from './components/RootLayout';
 
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route index element={<Dashboard /> } ></Route>
-      <Route path="/cart" element={<Cart /> } ></Route>
-    </Route>
-  ))
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
